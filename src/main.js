82359fd6cc8b136ac7a93fe7f242bb822583277b
wupeiwen <javapeiwen2010@gmail.com>
2018-12-27 10:48:17 +0800

    plugins: 调整elemnet-ui的引入方式

--- src/main.js
+++ src/main.js
@@ -2,10 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import '@/plugins/element-ui.js'
+
 Vue.config.productionTip = false
-Vue.use(ElementUI)
 
 new Vue({
   router,
