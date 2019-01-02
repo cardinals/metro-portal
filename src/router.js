50ce812ed3711783ac9b904e127828f67f96e966
wupeiwen <javapeiwen2010@gmail.com>
2019-01-02 11:06:01 +0800

    refactor: 优化router模块

--- src/router.js
+++ src/router.js
@@ -1,32 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/home',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/edit',
-      name: 'edit',
-      component: () => import('./views/edit.vue')
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('./views/login.vue')
+  mode: 'hash',
+  routes: [{
+    path: '/',
+    redirect: {
+      name: 'home'
     }
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: () => import('./views/Home.vue')
+  },
+  {
+    path: '/edit',
+    name: 'edit',
+    component: () => import('./views/edit.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('./views/login.vue')
+  }
   ]
 })
