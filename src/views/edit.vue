ce164158a3dddba8efcb7d277e7315d7f178034c
wupeiwen <javapeiwen2010@gmail.com>
2019-01-28 15:43:26 +0800

    refactor: 删除冗余代码

--- src/views/edit.vue
+++ src/views/edit.vue
@@ -506,23 +506,6 @@ export default {
       const property = column['property']
       return row[property].split('-')[1] === value
     },
-    // base64解码
-    base64ToBlob (urlData, type) {
-      let arr = urlData.split(',')
-      let mime = arr[0].match(/:(.*?);/)[1] || type
-      // 去掉url的头，并转化为byte
-      let bytes = window.atob(arr[1])
-      // 处理异常,将ascii码小于0的转换为大于0
-      let ab = new ArrayBuffer(bytes.length)
-      // 生成视图（直接针对内存）：8位无符号整数，长度1个字节
-      let ia = new Uint8Array(ab)
-      for (let i = 0; i < bytes.length; i++) {
-        ia[i] = bytes.charCodeAt(i)
-      }
-      return new Blob([ab], {
-        type: mime
-      })
-    },
     // 显示图片filter
     imgFilter (val) {
       return 'data:image/png;base64,' + val
